b8ebe09ce44e0fcfa077151d04d6190f0943a6ea
huseynovderya <61548937+huseynovderya@users.noreply.github.com>
2023-12-18 10:49:38 +0400

    Update Magic 8-ball.py

--- Magic 8-ball.py
+++ Magic 8-ball.py
@@ -1,43 +1,45 @@
-name = "Derya"
-question = "Will i be reach in future?"
-answer= ""
-
 import random
 
-random_number = random.randint(1, 9)
+name = "Derya"
 
+question = "A"
+
+answer = ""
+
+random_number = random.randint(1, 10)
+
+#print(random_number)
 
 if random_number == 1:
   answer = "Yes - definitely"
-
 elif random_number == 2:
   answer = "It is decidedly so"
-
 elif random_number == 3:
   answer = "Without a doubt"
-
 elif random_number == 4:
   answer = "Reply hazy, try again"
-
 elif random_number == 5:
   answer = "Ask again later"
-
 elif random_number == 6:
   answer = "Better not tell you now"
-
 elif random_number == 7:
   answer = "My sources say no"
-
 elif random_number == 8:
   answer = "Outlook not so good"
-
 elif random_number == 9:
   answer = "Very doubtful"
-
-
+elif random_number == 10:
+  answer = "Never"
 else:
-  print("Error")
+  answer = "Error"
 
-print(name + " asks: " + question)
-print("Magic 8-Ball's answer: " + answer)
-
+if name == " ":
+  name = "Question"
+  print(name, ":" , question)
+  print("Magic 8-Ball's answer:", answer)
+elif question == "":
+  print("Question is empty")
+else:
+   name = name
+   print(name, "asks:", question)
+   print("Magic 8-Ball's answer:", answer)
